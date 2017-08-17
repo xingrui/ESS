a97cfbeb2c4dcad10c4b52079aebb22d3a08feb8
xingrui <xingrui05@gmail.com>
2017-08-16 17:31:55 -0700

    1. add includeantruntime="on" to build.xml to avoid compile complain. 2. add solution for ant junit env problem.

--- init_env.sh
+++ init_env.sh
@@ -2,3 +2,6 @@
 #build.xml should only do compile pack things, not including inv init.
 
 #2. using mysql is too high weighted.
+
+#3. org.apache.tools.ant.taskdefs.optional.junit.JUnitTask was not found
+# solution : yum install ant*
