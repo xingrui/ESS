cf5b29b0e188aaaf74445e8828a5697d6a8db64b
xingrui <xingrui05@gmail.com>
2017-08-18 11:28:49 +0800

    add AbstractSqlDatabase.java class.

--- src/database/DatabaseMysqlImp.java
+++ src/database/DatabaseMysqlImp.java
@@ -3,14 +3,13 @@ package src.database;
 import java.sql.*;
 import java.util.Vector;
 import javax.swing.*;
-import src.database.Database;
+import src.database.AbstractSqlDatabase;
 
 /**
  * This class process all the method of update require and delete of the data in
  * the database.
  */
-public class DatabaseMysqlImp implements Database {
-    private Statement stmt;
+public class DatabaseMysqlImp extends AbstractSqlDatabase {
     private static final Database imp = new DatabaseMysqlImp();
     public static Database getInstance()
     {
@@ -36,164 +35,6 @@ public class DatabaseMysqlImp implements Database {
                                           + "or you haven't create database ess!"
                                           + "Please Check it");
             System.exit(0);
-        }
-    }
-
-    /** Getting records of elevator by elevatorID. */
-    synchronized public Vector<String> getRecords(int elevatorID)
-    {
-        String sql = "select * from control where elevatorID = " + elevatorID;
-        Vector<String> v = new Vector<String>();
-
-        try {
-            ResultSet rs = stmt.executeQuery(sql);
-
-            ResultSetMetaData md = rs.getMetaData();
-
-            int col = md.getColumnCount();
-            String s = "";
-
-            for (int i = 1; i <= col; i++) {
-                s += md.getColumnName(i) + "\t";
-
-                if (i == 2)
-                    s += "\t";
-            }
-
-            v.add(s);
-
-            while (rs.next()) {
-                s = "";
-
-                for (int i = 1; i <= col; i++) {
-                    s += rs.getString(i) + "\t";
-                }
-
-                v.add(s);
-            }
-
-        } catch (SQLException e) {
-            e.printStackTrace();
-        }
-
-        return v;
-    }
-
-    /** Judge whether a person is Administrator or not. */
-    synchronized public boolean isAdmin(String a, String b)
-    {
-        String sql = "select * from admin where adminname = \'" + a
-                     + "\'and password='" + b + "'";
-
-        try {
-            ResultSet rs = stmt.executeQuery(sql);
-
-            if (rs.next())
-                return true;
-        } catch (SQLException e) {
-            e.printStackTrace();
-        }
-
-        return false;
-    }
-
-    /** Add a outer request to database. */
-    synchronized public void addFloorRequest(int a, String type)
-    {
-        String sql = "insert into request values(" + a + ",'" + type + "','"
-                     + new Date(System.currentTimeMillis()) + " "
-                     + new Time(System.currentTimeMillis()) + "')";
-
-        try {
-            stmt.executeUpdate(sql);
-        } catch (SQLException e) {
-            System.out.println("Error in addFloorRequest");
-            e.printStackTrace();
-        }
-    }
-
-    /** Get the outer request of the elevator system. */
-    synchronized public Vector<String> getFloorRequest()
-    {
-        Vector<String> v = new Vector<String>();
-        String sql = "select * from request ";
-
-        try {
-            ResultSet rs = stmt.executeQuery(sql);
-
-            ResultSetMetaData md = rs.getMetaData();
-
-            int col = md.getColumnCount();
-            String s = "";
-
-            for (int i = 1; i <= col; i++) {
-                s += md.getColumnName(i) + "\t";
-            }
-
-            v.add(s);
-
-            while (rs.next()) {
-                s = "";
-
-                for (int i = 1; i <= col; i++) {
-                    s += rs.getString(i) + "\t";
-                }
-
-                v.add(s);
-            }
-
-        } catch (SQLException e) {
-            e.printStackTrace();
-        }
-
-        return v;
-    }
-
-    /** Add a inner request to database. */
-    synchronized public void addControlRecords(int elevatorID,
-            String event)
-    {
-        String sql = "insert into control values(" + elevatorID + ",'"
-                     + new Date(System.currentTimeMillis()) + " "
-                     + new Time(System.currentTimeMillis()) + "','" + event + "')";
-
-        try {
-            stmt.executeUpdate(sql);
-        } catch (SQLException e) {
-            e.printStackTrace();
-        }
-    }
-
-    /** Get the currentStatus of the elevator by elevatorID. */
-    synchronized public String getStatus(int elevatorID)
-    {
-        String sql = "select status from status where elevatorID = "
-                     + elevatorID;
-        String s = "";
-
-        try {
-            ResultSet rs = stmt.executeQuery(sql);
-
-            if (rs.next())
-                s += rs.getString(1);
-        } catch (SQLException e) {
-            e.printStackTrace();
-        }
-
-        return s;
-    }
-
-    /** Update the status of the elevator by elevatorID. */
-    synchronized public void updateStatus(int i, String status,
-                                          int currentFloor)
-    {
-        String sql = "update status set status='" + status + "',currentfloor="
-                     + currentFloor + " where elevatorID = " + i;
-
-        try {
-            stmt.executeUpdate(sql);
-        } catch (SQLException e) {
-            e.printStackTrace();
         }
     }
 }
